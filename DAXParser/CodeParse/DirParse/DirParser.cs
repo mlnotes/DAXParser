823234f8050ca60ee1e17eb2066a82782f7298c1
Zhu Hanfeng <zhuhanfeng@gmail.com>
2014-04-02 17:24:28 +0800

    make the process of parsing each layer files be parallel and remove the risk of invalid dir

--- DAXParser/CodeParse/DirParse/DirParser.cs
+++ DAXParser/CodeParse/DirParse/DirParser.cs
@@ -7,17 +7,29 @@ using System.Threading;
 
 namespace DAXParser.CodeParse.DirParse
 {
-	class ParmObject<T>
-		where T: BaseObjectData
+	class IntRef
 	{
-		public ParmObject(int count, string path, Func<string, T> func, 
+		public int Value { get; set; }
+
+		public IntRef(int val = 0)
+		{
+			this.Value = val;
+		}
+	}
+
+	class ParmObject<T>
+		where T : BaseObjectData
+	{
+		public ParmObject(int count, IntRef processed, string path, Func<string, T> func,
 							Dictionary<string, string> prefixOwnership,
 							Dictionary<string, string> postfixOwnership,
 							Dictionary<string, string> country,
 							Dictionary<string, string> region,
-							List<T> objects, ManualResetEvent handle)
+							Dictionary<string, T> objects,
+							ManualResetEvent handle)
 		{
 			this.Count = count;
+			this.Processed = processed;
 			this.Path = path;
 			this.ParseFunc = func;
 			this.Objects = objects;
@@ -29,9 +41,10 @@ namespace DAXParser.CodeParse.DirParse
 		}
 
 		public int Count { get; set; }
+		public IntRef Processed { get; set; }
 		public string Path { get; set; }
 		public Func<string, T> ParseFunc { get; set; }
-		public List<T> Objects { get; set; }
+		public Dictionary<string, T> Objects { get; set; }
 		public Dictionary<string, string> PrefixOwnership { get; set; }
 		public Dictionary<string, string> PostfixOwnership { get; set; }
 		public Dictionary<string, string> Country { get; set; }
@@ -42,9 +55,9 @@ namespace DAXParser.CodeParse.DirParse
 	class DirParser
 	{
 
-		protected static void AssignOwnerAndRegion<T>(T obj, Dictionary<string, string> prefix, 
+		protected static void AssignOwnerAndRegion<T>(T obj, Dictionary<string, string> prefix,
 								Dictionary<string, string> postfix, Dictionary<string, string> region)
-			where T:BaseObjectData
+			where T : BaseObjectData
 		{
 			if (prefix != null && prefix.Count > 0)
 			{
@@ -87,7 +100,7 @@ namespace DAXParser.CodeParse.DirParse
 		}
 
 		protected static void AssignCountry<T>(T obj, Dictionary<string, string> country)
-			where T:BaseObjectData
+			where T : BaseObjectData
 		{
 			if (country != null && country.Count > 0)
 			{
@@ -105,42 +118,55 @@ namespace DAXParser.CodeParse.DirParse
 			}
 		}
 
-		protected static void ParseFirstLayerFile<T>(ParmObject<T> obj)
-			where T:BaseObjectData
+
+		protected static void ParseLayerFile<T>(ParmObject<T> obj)
+			where T : BaseObjectData
 		{
 			T data = obj.ParseFunc(obj.Path);
 			AssignOwnerAndRegion(data, obj.PrefixOwnership, obj.PostfixOwnership, obj.Region);
 			AssignCountry(data, obj.Country);
+
+			// only lock on the same object can prevent concurrency
+			// all ParmObject instances share the same objects
 			lock (obj.Objects)
 			{
-				obj.Objects.Add(data);
-				if (obj.Count == obj.Objects.Count)
+				string key = data.Name.ToUpper();
+				if (obj.Objects.ContainsKey(key))
+				{
+					obj.Objects[key].MergeWith(data);
+				}
+				else
+				{
+					obj.Objects[key] = data;
+				}
+
+				obj.Processed.Value++;
+				if (obj.Processed.Value >= obj.Count)
 				{
 					obj.Handle.Set();
 				}
 			}
 		}
 
-		protected static void AddObject<T>(List<T> objects, T obj, Dictionary<string, string> prefix, 
-						Dictionary<string, string> postfix, Dictionary<string, string> country,
-						Dictionary<string, string> region)
-			where T : BaseObjectData
-		{
-			AssignOwnerAndRegion(obj, prefix, postfix, region);
-			AssignCountry(obj, country);
-			objects.Add(obj);
-		}
-
 		protected static FileInfo[] GetFiles(string layerPath, string module, string pattern)
 		{
 			string path = Path.Combine(layerPath, module);
 			DirectoryInfo dir = new DirectoryInfo(path);
-			return dir.GetFiles(pattern);
+
+			// in case of invalid dir
+			try
+			{
+				return dir.GetFiles(pattern);
+			}
+			catch (Exception e)
+			{
+				return new FileInfo[0];
+			}
 		}
 
 		public static List<T> Parse<T>(string[] layerPaths, string module, Dictionary<string, string> prefix,
 										Dictionary<string, string> postfix, Dictionary<string, string> country,
-										Dictionary<string, string> region, Func<string, T> parseFunc, 
+										Dictionary<string, string> region, Func<string, T> parseFunc,
 										string pattern = "*.xpo")
 			where T : BaseObjectData
 		{
@@ -149,62 +175,24 @@ namespace DAXParser.CodeParse.DirParse
 				return new List<T>();
 			}
 
-			FileInfo[] files = GetFiles(layerPaths[0], module, pattern);
-			List<T> objects = new List<T>();
-
-			// Get all objects in the first layer
-			ManualResetEvent[] handles = new ManualResetEvent[]{new ManualResetEvent(false)};
-			foreach (FileInfo file in files)
+			Dictionary<string, T> objects = new Dictionary<string, T>();
+			IntRef processed = new IntRef(0);
+			ManualResetEvent[] handles = new ManualResetEvent[] { new ManualResetEvent(false) };
+			foreach (string layerPath in layerPaths)
 			{
-				//T data = parseFunc(file.FullName);
-				//AddObject(objects, data, ownership);
-
-				ParmObject<T> parm = new ParmObject<T>(files.Length, file.FullName, parseFunc, 
-											prefix, postfix, country, region, objects, handles[0]);
-				ThreadPool.QueueUserWorkItem(obj => ParseFirstLayerFile<T>(obj as ParmObject<T>), parm);
-			}
-
-			WaitHandle.WaitAll(handles);
-
-			// merge objects in upper layers
-			Dictionary<string, T> upperLayers = new Dictionary<string, T>();
-			for (int i = 1; i < layerPaths.Length; ++i)
-			{
-				files = GetFiles(layerPaths[i], module, pattern);
+				FileInfo[] files = GetFiles(layerPath, module, pattern);
+				processed.Value = 0;
+				handles[0].Reset();
 				foreach (FileInfo file in files)
 				{
-					T data = parseFunc(file.FullName);
-					string key = data.Name.ToUpper();
-					if (upperLayers.ContainsKey(key))
-					{
-						upperLayers[key].MergeWith(data);
-					}
-					else
-					{
-						upperLayers[key] = data;
-					}
+					ParmObject<T> parm = new ParmObject<T>(files.Length, processed, file.FullName, parseFunc,
+											prefix, postfix, country, region, objects, handles[0]);
+					ThreadPool.QueueUserWorkItem(obj => ParseLayerFile<T>(obj as ParmObject<T>), parm);
 				}
+				WaitHandle.WaitAll(handles);
 			}
 
-			// merge objects that exist in first layer
-			for (int i = 0; i < objects.Count; ++i)
-			{
-				T data = objects[i];
-				string key = data.Name.ToUpper();
-				if (upperLayers.ContainsKey(key))
-				{
-					data.MergeWith(upperLayers[key]);
-					upperLayers.Remove(key);
-				}
-			}
-
-			// mrege objects that do not exist in the first layer
-			foreach (T data in upperLayers.Values)
-			{
-				AddObject(objects, data, prefix, postfix, country, region);
-			}
-
-			return objects;
+			return objects.Values.ToList();
 		}
 	}
 }
